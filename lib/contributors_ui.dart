d76fe44a3bd3e1fcd00a0c9ec03fae09aadbd940
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-03-22 18:45:23 +0300

    ⬆ Update example

--- lib/contributors_ui.dart
+++ lib/contributors_ui.dart
@@ -1,16 +1,3 @@
 library contributer_ui;
 
 export 'package:contributors_ui/src/ui/views/contributors/contributors_view.dart';
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class ContributorsUi {
-  static const MethodChannel _channel = const MethodChannel('contributors_ui');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
