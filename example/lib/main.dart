9759eca050a95bae680e085c1f4d07631846164e
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-03-25 19:06:11 +0300

    🔥 Make example code better

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,11 +17,88 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ContributorsView(
-      ownerName: "Flutter-Buddies",
-      repoName: "contributors",
-      locale: Locale("en"), // try this for english language
-      // locale: Locale("ar"), // try this for arabic language
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Contributors Example",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ReadyUI(),
+                  ),
+                );
+              },
+              child: Text("Ready UI"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CustomUI(),
+                  ),
+                );
+              },
+              child: Text("Custom UI"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReadyUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ContributorsView(
+        ownerName: "Flutter-Buddies",
+        repoName: "contributors",
+        locale: Locale("en"), // try this for english language
+        // locale: Locale("ar"), // try this for arabic language
+      ),
+    );
+  }
+}
+
+class CustomUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ContributorsBuilderView(
+        ownerName: "Flutter-Buddies",
+        repoName: "contributors",
+        locale: Locale("en"), // try this for english language
+        // locale: Locale("ar"), // try this for arabic language
+        builder: (
+          BuildContext context,
+          List<Contributor> contributors,
+        ) {
+          return ListView.builder(
+            itemCount: contributors.length,
+            itemBuilder: (
+              BuildContext context,
+              int index,
+            ) {
+              final Contributor contributor = contributors[index];
+
+              return Text(contributor.login);
+            },
+          );
+        },
+      ),
     );
   }
 }
--- lib/contributors.dart
+++ lib/contributors.dart
@@ -1,3 +1,7 @@
 library contributors;
 
 export 'package:contributors/src/ui/views/contributors/contributors_view.dart';
+export 'package:contributors/src/ui/views/contributors_builder/contributors_builder_view.dart';
+export 'package:contributors/src/ui/widgets/contributor_list_tile.dart';
+
+export 'package:github/github.dart';
